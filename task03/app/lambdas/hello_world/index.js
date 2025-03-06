2eda782560679639dd8e124781ec5fdacfe10d6d
Arif Bashir Dar <arifbashir_dar@epam.com>
2025-03-06 11:15:47 +0530

    Solve

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,8 +1,8 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
+    console.log("Received event:", JSON.stringify(event, null, 2));
+    return {
         statusCode: 200,
-        message: JSON.stringify('Hello from Lambda'),
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: "Hello from Lambda" })
     };
-    return response;
 };
